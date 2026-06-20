0e4d1a3982fa9381bff28b00d48f0aae407a70e4
Alexandr Feofanov <113620461+usersasha18@users.noreply.github.com>
2026-06-20 21:03:30 +1000

    Create final.js

---
+++ final.js
@@ -0,0 +1,23 @@
+
+function showFinalModal() {
+    
+    const user = localStorage.getItem("username") || "Пользователь";
+
+    const element = document.querySelector('#score-content')
+    const jsonString = localStorage.getItem('quizResult');
+    const myObject = JSON.parse(jsonString);
+    console.log(myObject);
+    
+        element.innerHTML = `
+        <div class="score-content">
+            <h2>🏁 Квиз завершён</h2>
+
+            <p><b>${myObject.name}</b></p>
+            <p>Всего вопросов ${myObject.score}</p>
+            <p>✔ Правильных ответов: ${myObject.score - myObject.wrong}</p>
+            <p>❌ Неправильных ответов: ${myObject.wrong}</p>
+        </div>
+    `;
+}
+
+showFinalModal()
